6e1b29b23be6f36b718757197e592e2971c4d30c
jmllegrand <jml.legrand@gmail.com>
2016-11-11 19:06:15 +0100

    day2 (section 3): Display a list of items (using *ngFor) - Display / style conditionnally

--- README.md
+++ README.md
@@ -50,14 +50,14 @@ export class Wine {
 }
 ```
 
-- instanciate classes
+- Instanciate classes
 ```
     wine: Wine = new Wine(1, "Chateau Latour 2015");
 ```
 
-- use template strings feature in ES2015
+- Use template strings feature in ES2015
 
-- use Two-Way Binding (only when necessary)
+- Use Two-Way Binding (only when necessary)
 ```
  <input [(ngModel)]="wine.name" placeholder="name">
 ```
@@ -70,4 +70,83 @@ import the FormsModule package
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
+``
+
+### Day 2 - section 3
+
+- Create an typed array
 ```
+const WINES : Wine[] = [
+    new Wine(0, "Chateau Ausone"),
+    new Wine(1, "Chateau Figeac"),
+    new Wine(2, "Chateau Canon"),
+    new Wine(3, "Chateau Fleur Cardinale"),
+    new Wine(4, "Chateau Grand Corbin-Despagne"),
+    new Wine(5, "Chateau Petrus"),
+    new Wine(6, "Chateau Eglise Clinet"),
+    new Wine(7, "Chateau L'Evangile"),
+];
+```
+
+- Display list items in the template using *ngFor
+```
+        <ul class="wines">
+            <li *ngFor="let wine of wines">
+                <span class="badge">{{wine.id}}</span>
+                {{wine.name}}
+            </li>
+        </ul>
+```
+
+- Style the component using the styles property on the @Component decorator
+```
+ styles: [ `
+      .selected {
+        background-color: #CFD8DC !important;
+        color: white;
+      }
+      .wines {
+        margin: 0 0 2em 0;
+        list-style-type: none;
+        padding: 0;
+        width: 15em;
+      }
+       `
+ ]
+```
+
+Note : When we assign styles to a component they are scoped to that specific component.
+Our styles will only apply to our AppComponent and won't "leak" to the outer HTML.
+
+- Use the click event (to select an item in a list)
+```
+            <li *ngFor="let wine of wines" (click)="onSelect(wine)">
+                <span class="badge">{{wine.id}}</span>
+                {{wine.name}}
+            </li>
+```
+
+- Implement an event handler
+```
+    onSelect = (wine: Wine) => {
+        this.selectedWine = wine;
+    }
+```
+
+- Display conditionnally a block using *ngIf
+```
+ <div *ngIf="selectedWine">
+ </div>
+
+
+- Style a block conditionnally using property binding
+```
+            <li *ngFor="let wine of wines"  [class.selected]="wine === selectedWine" (click)="onSelect(wine)">
+                <span class="badge">{{wine.id}}</span>
+                {{wine.name}}
+            </li>
+```
+This says that “apply the selected class if the heroes match, remove it if they don’t”.
+
+
+
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,23 +3,101 @@
  */
 
 import {Component} from "@angular/core";
-import {Wine} from "./Wine";
+import {Wine} from "./wine";
+
+
+const WINES : Wine[] = [
+    new Wine(0, "Chateau Ausone"),
+    new Wine(1, "Chateau Figeac"),
+    new Wine(2, "Chateau Canon"),
+    new Wine(3, "Chateau Fleur Cardinale"),
+    new Wine(4, "Chateau Grand Corbin-Despagne"),
+    new Wine(5, "Chateau Petrus"),
+    new Wine(6, "Chateau Eglise Clinet"),
+    new Wine(7, "Chateau L'Evangile"),
+];
 
 @Component({
     selector: 'my-app',
+    styles: [ `
+      .selected {
+        background-color: #CFD8DC !important;
+        color: white;
+      }
+      .wines {
+        margin: 0 0 2em 0;
+        list-style-type: none;
+        padding: 0;
+        width: 15em;
+      }
+      .wines li {
+        cursor: pointer;
+        position: relative;
+        left: 0;
+        background-color: #EEE;
+        margin: .5em;
+        padding: .3em 0;
+        height: 1.6em;
+        border-radius: 4px;
+      }
+      .wines li.selected:hover {
+        background-color: #BBD8DC !important;
+        color: white;
+      }
+      .wines li:hover {
+        color: #607D8B;
+        background-color: #DDD;
+        left: .1em;
+      }
+      .wines .text {
+        position: relative;
+        top: -3px;
+      }
+      .wines .badge {
+        display: inline-block;
+        font-size: small;
+        color: white;
+        padding: 0.8em 0.7em 0 0.7em;
+        background-color: #607D8B;
+        line-height: 1em;
+        position: relative;
+        left: -1px;
+        top: -4px;
+        height: 1.8em;
+        margin-right: .8em;
+        border-radius: 4px 0 0 4px;
+      }
+    `
+    ],
     template: `
         <h1>{{title}}</h1>
-        <h2>{{wine.name}} details!</h2>
-        <div> <label>id: </label> {{wine.id}} </div>
-        <div> 
-            <label>name: </label> 
-            <input [(ngModel)]="wine.name" placeholder="name">
+        <ul class="wines">
+            <li *ngFor="let wine of wines"  [class.selected]="wine === selectedWine" (click)="onSelect(wine)">
+                <span class="badge">{{wine.id}}</span>
+                {{wine.name}}
+            </li>
+        </ul>
+        
+        <div *ngIf="selectedWine">
+            <h2> Details for {{selectedWine.name}} :</h2>
+            <div> <label>id: </label> {{selectedWine.id}} </div>
+            <div> 
+                <label>name: </label> 
+                <input [(ngModel)]="selectedWine.name" placeholder="name">
+            </div>
         </div>
+
+        
     `
 })
 
 
 export class AppComponent {
-    wine: Wine = new Wine(1, "Chateau Latour 2015");
+    wines : Wine[] =  WINES;
     title: string = "List of wines";
+    selectedWine: Wine;
+
+    onSelect = (wine: Wine) => {
+        this.selectedWine = wine;
+    }
 }
