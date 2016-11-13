ecbe954d62df609f0b7c3186b3c6fc25e7a767e8
jmllegrand <jml.legrand@gmail.com>
2016-11-13 22:11:24 +0100

    day2 (section 6): Refine DashboardComponent

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,12 +1,101 @@
 /**
  * Created by jmlegrand on 13/11/16.
  */
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {WineService} from "./wine.service";
+import {Wine} from "./wine";
 
 @Component({
     selector: "dashboard",
-    template: `<h2>This is the dashboard</h2>`
-})
+    styles: [
+        `
+        [class*='col-'] {
+          float: left;
+          padding-right: 20px;
+          padding-bottom: 20px;
+        }
+        [class*='col-']:last-of-type {
+          padding-right: 0;
+        }
+        a {
+          text-decoration: none;
+        }
+        *, *:after, *:before {
+          -webkit-box-sizing: border-box;
+          -moz-box-sizing: border-box;
+          box-sizing: border-box;
+        }
+        h3 {
+          text-align: center; margin-bottom: 0;
+        }
+        h4 {
+          position: relative;
+        }
+        .grid {
+          margin: 0;
+        }
+        .col-1-4 {
+          width: 25%;
+        }
+        .module {
+          padding: 20px;
+          text-align: center;
+          color: #eee;
+          max-height: 120px;
+          min-width: 120px;
+          background-color: #607D8B;
+          border-radius: 2px;
+        }
+        .module:hover {
+          background-color: #EEE;
+          cursor: pointer;
+          color: #607d8b;
+        }
+        .grid-pad {
+          padding: 10px 0;
+        }
+        .grid-pad > [class*='col-']:last-of-type {
+          padding-right: 20px;
+        }
+        @media (max-width: 600px) {
+          .module {
+            font-size: 10px;
+            max-height: 75px; }
+        }
+        @media (max-width: 1024px) {
+          .grid {
+            margin: 0;
+          }
+          .module {
+            min-width: 60px;
+          }
+        }`
+    ],
+    template: `
+        <h3>Top Wines</h3>
+        <div class="grid grid-pad">
+            <div *ngFor="let wine of wines" class="col-1-4">
+                <div class="module wine">
+                    <h4>{{wine.name}}</h4>
+                </div>
+            </div>
+        </div>
+    
+        `
+    })
 
 
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+
+    constructor(private wineService: WineService) { }
+
+    ngOnInit(): void {
+        this.wineService.getWines()
+            .then((wines) =>
+                this.wines = wines.slice(0,4)
+            )
+    }
+
+    public wines: Wine[] = [];
+
+}
